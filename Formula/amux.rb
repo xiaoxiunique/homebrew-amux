b208820c8ccded7d29ec92af51fa63f75119b564
Atom <929746602@qq.com>
2026-07-09 15:28:27 +0800

    amux 0.4.1

--- Formula/amux.rb
+++ Formula/amux.rb
@@ -1,8 +1,8 @@
 class Amux < Formula
   desc "Run AI coding agents in per-directory persistent tmux sessions"
   homepage "https://github.com/xiaoxiunique/amux"
-  url "https://github.com/xiaoxiunique/amux/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "72c3479dc634f7a72b398668df686dce1d6fedc964b8ed1967893f2c082c4a69"
+  url "https://github.com/xiaoxiunique/amux/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "660b3fe591eddab4b641b4579dbc07c59f9d37b13d76af68f5950bbbff5c5d9a"
   license "MIT"
   depends_on "rust" => :build
   depends_on "tmux"
