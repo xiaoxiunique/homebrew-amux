ad0ec6c9a24d028f8945e0d10e1c45f0fafe756e
Atom <929746602@qq.com>
2026-07-14 12:44:52 +0800

    amux 0.5.3 sha fix

--- Formula/amux.rb
+++ Formula/amux.rb
@@ -2,7 +2,7 @@ class Amux < Formula
   desc "Run AI coding agents in per-directory persistent tmux sessions"
   homepage "https://github.com/xiaoxiunique/amux"
   url "https://github.com/xiaoxiunique/amux/archive/refs/tags/v0.5.3.tar.gz"
-  sha256 "408cc8a4fba9a54ede7be1ecb98793f6c07d330c177b698fa6e2bdf80591f6ef"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
   depends_on "rust" => :build
   depends_on "rmux"
