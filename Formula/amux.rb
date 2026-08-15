29bf7301ede3c47b0bd45b1ee2df23e5564f7c8d
Atom <929746602@qq.com>
2026-08-15 17:34:06 +0800

    amux 0.5.21

--- Formula/amux.rb
+++ Formula/amux.rb
@@ -1,18 +1,18 @@
 class Amux < Formula
   desc "Run AI coding agents in per-directory persistent rmux sessions"
   homepage "https://github.com/xiaoxiunique/amux"
-  version "0.5.20"
+  version "0.5.21"
   license "MIT"
   depends_on "rmux"
 
   on_macos do
     on_arm do
-      url "https://github.com/xiaoxiunique/amux/releases/download/v0.5.20/amux-v0.5.20-aarch64-apple-darwin.tar.gz"
-      sha256 "4f6756e0c64d2d912c03f9f188d73b47c9790a50b490872dd46d25b71d9898e1"
+      url "https://github.com/xiaoxiunique/amux/releases/download/v0.5.21/amux-v0.5.21-aarch64-apple-darwin.tar.gz"
+      sha256 "335d3d817996210244d7e15d6552f4499fdb9f49f5c5d77b7982018c4ba91a35"
     end
     on_intel do
-      url "https://github.com/xiaoxiunique/amux/releases/download/v0.5.20/amux-v0.5.20-x86_64-apple-darwin.tar.gz"
-      sha256 "a57c6b2dbdf9c23038b5211b3a23a61bc4cef1f6062bd0d77f40a616afbe694c"
+      url "https://github.com/xiaoxiunique/amux/releases/download/v0.5.21/amux-v0.5.21-x86_64-apple-darwin.tar.gz"
+      sha256 "026a672ae399bc1bf1df3a1aaf1d653c5910b5e6da0895a751d6e1904a4099cf"
     end
   end
 
